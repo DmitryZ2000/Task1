a5f17d64841cffcbf91f11b6789846d29ec0c537
Dmitry <orionvega5@gmail.com>
2022-09-17 23:01:18 +0300

    Add final code and draw

--- Program.cs
+++ Program.cs
@@ -6,13 +6,48 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"] 
 // ["Russia", "Deпmark", "Kazan"] -> []
 
-void PrintArray(string[] array)
+string[] SortAndCreatNewArray(string[] array)
 {
+    string[] newArray = new string[array.Length];
+    int j = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
     }
+    return newArray;
 }
 
-string[] array = {"Russia", "Deпmark", "Kazan", "123"};
+string[] ResizeArray(string[] newArray)
+{
+    int newLengthArray = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if(newArray[i] != null)
+        {
+            newLengthArray++;
+        }
+    }
+    Array.Resize(ref newArray, newLengthArray);
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    if (array.Length > 0 ) Console.WriteLine($"{array[array.Length-1]}]"); //Защита от пустого массива
+    else Console.WriteLine("]"); 
+}
+
+string[] array = { "Russia", "Volgograd", "Kazan", "123"};  // Начальный массив
 PrintArray(array);
+string[] newArray = SortAndCreatNewArray(array);
+newArray = ResizeArray(newArray);
+PrintArray(newArray);
